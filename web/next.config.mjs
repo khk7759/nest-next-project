9e01e3213df15e8532e364acb8ad6ac398ecac08
khk7759 <khk7759@naver.com>
2026-02-17 19:32:51 +0900

    feat: Nextjs 설정 추가
    - rewrite
    - remotePatterns

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/',
+                destination: '/challenges',
+            },
+        ];
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
